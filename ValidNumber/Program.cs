9c220b26fc8a8e7a2488d6bea8adb2366e83dd0c
Matilda Mared <matildamared@live.se>
2022-02-26 13:45:48 +0100

    add ValidNumber exercise

---
+++ ValidNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Can you enter a valid number? 😱");
+Console.WriteLine("Please enter a number between 1 and 10:");
+
+var userInput = Console.ReadLine();
+var number = int.Parse(userInput);
+
+if (number > 0 && number < 10) {
+	Console.WriteLine("Wow! You entered a valid number, good job! 🎉");
+}
+else {
+	Console.WriteLine("You can't read simple instructions now, can you? 🤔");
+}
