95335b6cfa546b7937f238cd6f63c0217986e8c3
Matilda Mared <matildamared@live.se>
2022-02-26 13:55:57 +0100

    add MaxNumber exercise

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello friend! 😀 This is a program that takes to numbers and displays the maximum of the two. Neat huh? ☺️");
+
+Console.WriteLine("\nPlease enter the first number:");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nThank you! 🙏");
+
+Console.WriteLine("\nPlease enter the second number:");
+
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nThank you once again! Have a cookie! 🍪");
+
+var maxValue = Math.Max(firstNumber, secondNumber);
+
+Console.WriteLine("\nSo for the results... *drumroll* 🥁");
+Console.WriteLine($"Your inputs are {firstNumber} and {secondNumber}.");
+Console.WriteLine($"The maximum of the two is... {maxValue}! 🥳");
+Console.WriteLine("\nThank you for using the program, have a nice day! 🤗");
