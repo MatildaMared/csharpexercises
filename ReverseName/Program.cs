6f2df1167573d4e759dd472a05546506b389eb4b
Matilda Mared <matildamared@live.se>
2022-02-27 15:40:42 +0100

    complete ReverseName exercise

---
+++ ReverseName/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Write a program and ask the user to enter their name. 
+Use an array to reverse the name and then store 
+the result in a new string. Display the 
+reversed name on the console.
+*/
+
+Console.Write("Hi! 💖 Please enter your name: ");
+var name = Console.ReadLine();
+
+var reversedName = "";
+
+for (var i = name.Length - 1; i >= 0; i--) {
+	reversedName += name[i];
+}
+
+Console.WriteLine($"\nThank you {name}! 🤗 Did you know that your name reversed is {reversedName}? 😄");
