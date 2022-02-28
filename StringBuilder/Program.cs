59920679ddc3dca5981621b83d8c8527264bab6b
Matilda Mared <matildamared@live.se>
2022-02-28 18:27:24 +0100

    work with StringBuilder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+var builder = new StringBuilder();
+builder
+	.Append('-', 10)
+	.AppendLine()
+	.Append("Header")
+	.AppendLine()
+	.Append('-', 10);
+
+Console.WriteLine(builder);
+
+builder.Replace('-', '+');
+Console.WriteLine(builder);
+
+builder.Remove(0, 10);
+Console.WriteLine(builder);
+
+builder.Insert(0, '*'.ToString(), 10);
+Console.WriteLine(builder);
+
+Console.WriteLine("First char: " + builder[0]);
