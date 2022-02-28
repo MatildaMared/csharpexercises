394b6c569cb03a665474cb2a4b90846b8820f5e5
Matilda Mared <matildamared@live.se>
2022-02-28 18:53:40 +0100

    complete Duplicates exercise

---
+++ Duplicates/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Write a program and ask the user to enter a few
+ * numbers separated by a hyphen. If the user simply
+ * presses Enter, without supplying an input, exit
+ * immediately; otherwise, check to see if there
+ * are duplicates. If so, display "Duplicate"
+ * on the console.
+ */
+
+Console.WriteLine("Enter a few numbers separated by a hyphen and I will check for duplicates! ☺️");
+Console.WriteLine("Press Enter without supplying an input to exit.");
+Console.WriteLine("Example: 12-17-64-9-3");
+Console.Write("\nEnter your numbers: ");
+
+var userInput = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userInput)) {
+    Console.WriteLine("\nYou didn't enter anything! Exiting...");
+    Environment.Exit(0);
+}
+
+var numbers = userInput.Split('-');
+
+if (numbers.Length != numbers.Distinct().Count()) {
+	Console.WriteLine("\nDuplicate numbers detected! ☹️");
+} else {
+	Console.WriteLine("\nNo duplicates detected! ☺️");
+}
