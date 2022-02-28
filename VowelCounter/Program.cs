02196f11a948580e2e00c09a4e9f9b4e80f16449
Matilda Mared <matildamared@live.se>
2022-02-28 19:18:21 +0100

    complete VowelCounter exercise

---
+++ VowelCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Write a program and ask the user to enter an English word.
+ * Count the number of vowels (a, e, o, u, i) in the word.
+ * So, if the user enters "inadequate", the program
+ * should display 6 on the console.
+*/
+
+char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+Console.WriteLine("Enter a word and I will count the number of vowels! 😊");
+Console.Write("\nYour word: ");
+
+var userInput = Console.ReadLine();
+
+var vowelCount = 0;
+
+var characters = userInput.ToCharArray();
+
+foreach (var character in characters) {
+    if (Array.IndexOf(vowels, character) > -1) {
+        vowelCount++;
+    }
+}
+
+Console.WriteLine("\nThere are {0} vowels in your word!", vowelCount);
