a243cded238969ce6420631485830c60c7c1c8a3
Matilda Mared <matildamared@live.se>
2022-02-28 19:00:23 +0100

    complete ValidTime exercise

---
+++ ValidTime/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Write a program and ask the user to enter
+ * a time value in the 24-hour time format (e.g. 19:00).
+ * A valid time should be between 00:00 and 23:59.
+ * If the time is valid, display "Ok"; otherwise,
+ * display "Invalid Time". If the user doesn't
+ * provide any values, consider it as invalid time.
+*/
+
+Console.Write("Please enter a time in the 24-hour format (e.g. 19:00): ");
+var time = Console.ReadLine();
+
+// Check if the time is valid
+var timeArray = time.Split(':');
+
+if (timeArray.Length == 2) {
+	var hour = int.Parse(timeArray[0]);
+	var minute = int.Parse(timeArray[1]);
+
+	if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59) {
+		Console.WriteLine("\nYou entered a valid time! 😄");
+	}
+	else {
+		Console.WriteLine("\nInvalid Time... 😢");
+	}
+} else {
+	Console.WriteLine("\nInvalid Time... 😢");
+}
